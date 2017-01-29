254cd28880dbb47b6e5ee3255624c6a726a74031
Lily Eom <lily.eom@kellogg.northwestern.edu>
2017-01-29 15:26:17 -0600

    shufflefix2

--- poker/poker.js
+++ poker/poker.js
@@ -32,15 +32,13 @@ window.getDeck = function() {
 $(function() {
   $(".deal").on("click", function(event) {
     event.preventDefault()
-    shuffleDeck = function() {
-      return getDeck().shuffle()
-    }
+    var shuffleDeck = getDeck().shuffle()
+    console.log(shuffleDeck)
 
-
-    $("#c1").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck()[0]+".png")
-    $("#c2").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck()[1]+".png")
-    $("#c3").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck()[2]+".png")
-    $("#c4").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck()[3]+".png")
-    $("#c5").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck()[4]+".png")
+    $("#c1").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck[0]+".png")
+    $("#c2").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck[1]+".png")
+    $("#c3").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck[2]+".png")
+    $("#c4").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck[3]+".png")
+    $("#c5").attr("src","http://golearntocode.com/images/cards/"+shuffleDeck[4]+".png")
   })
 })
